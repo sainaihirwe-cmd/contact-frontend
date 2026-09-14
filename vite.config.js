e6d9dc074fcf7ffb774635b4d2b95625844603ea
sainaihirwe-cmd <sainaihirwe@gmail.com>
2026-09-14 08:47:15 +0100

    feat:added new features

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3032',
+        target: 'http://localhost:5050',
         changeOrigin: true,
       },
     },
